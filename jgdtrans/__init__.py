44336ac4b64e8f052ad0ddc4337ec70894923334
Kentaro Tatsumi <paqira.2019@gmail.com>
2025-06-20 21:15:43 +0900

    bump to 0.3.0

--- jgdtrans/__init__.py
+++ jgdtrans/__init__.py
@@ -21,7 +21,7 @@ from .par import Parameter, ParData
 from .point import Point
 from .transformer import ParameterSet, Transformer, from_dict, load, loads
 
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 
 
 __all__ = [
